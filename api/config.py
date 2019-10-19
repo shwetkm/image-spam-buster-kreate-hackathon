42161f78f3617d64833b41a8ee9c9abc3742f323
shwetkm <shwetknit@gmail.com>
2019-10-20 02:21:28 +0530

    config file added

---
+++ api/config.py
@@ -0,0 +1,12 @@
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+
+cont_cols = ['ccs_char_count',
+            'ccs_word_count',
+            'ccs_word_density',
+            'ccs_punctuation_count',
+            'ccs_title_word_count',
+            'ccs_upper_case_word_count']
+
+            
